10b79ad12ab434545cfc242a38e1ae0cf1fd4e2f
Milena <milena181903@hotmail.com>
2025-02-17 17:35:43 -0300

    alterações para publicação herok

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,3 +7,7 @@ from app import routes
 
 if __name__ == "__main__":
     app.run()
+
+if __name__ == 'main' :
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port=port)
